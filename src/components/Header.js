ed41c4836272e65175f2e3fc60b49b5c4ac061a6
Jeff <jeffreyrjohnsonsemail@gmail.com>
2018-03-23 10:55:49 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Joe'
+    };
+  }
+  render() {
+    return (
+      <header>
+        <div className="logo">Logo</div>
+        <nav>
+          <a href="#">About Us</a>
+          <a href="#">Log in</a>
+          <a href="#" className="register-btn">
+            Register
+          </a>
+        </nav>
+      </header>
+    );
+  }
+}
